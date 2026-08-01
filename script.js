dc3db25131f4030777d4eda84af67b9b248fabb5
fady-albert <fadyalber2009@gmail.com>
2026-08-01 18:20:39 +0300

    add game title, welcome message, and reset functionality with styling updates

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 const modeBtn = document.getElementById('mode');
 const modeBtnIcon = document.querySelector('#mode span');
 const main = document.body;
+const cells = document.querySelectorAll('.cell');
+const msg = document.getElementById('message');
+const resetBtn = document.getElementById('reset');
 
 // add JS data
 const savedMode = localStorage.getItem('mode') || '';
+let player = 'O';
+let end = false;
 
 // mode function
 modeBtn.addEventListener('click', () => {
@@ -24,3 +29,83 @@ if(savedMode === 'dark') {
     main.classList.add('dark');
     modeBtnIcon.textContent = main.classList.contains('dark') ? 'dark_mode' : 'light_mode';
 }
+
+//! game function
+
+// reset function
+resetBtn.addEventListener('click', () => {
+    cells.forEach(cell => {
+        cell.textContent = '';
+        cell.style.border = 'none';
+    });
+    end = false;
+    msg.textContent = "Game reset!";
+});
+
+// draw
+function draw() {
+    if(!end){
+        // logic for handling a draw
+        const isDraw = Array.from(cells).every(cell => cell.textContent !== '');
+        if(isDraw) {
+            end = true;
+            msg.textContent = "It's a draw!";
+        }
+    }
+}
+
+// add border color to the winning cells
+function borderColor(el, color) {
+    el.style.border = `5px solid ${color}`;
+}
+
+// win
+function checkWin() {
+    const winCond = [
+        // row
+        [0, 1, 2],
+        [3, 4, 5],
+        [6 ,7, 8],
+
+        // column
+        [0, 3, 6],
+        [1 ,4 ,7],
+        [2 ,5, 8],
+
+        // diagonal
+        [0 ,4, 8],
+        [2, 4, 6]
+    ]
+
+    for(let win of winCond) {
+        const [a, b, c] = win;
+
+        if(cells[a].textContent === cells[b].textContent && cells[b].textContent === cells[c].textContent && cells[a].textContent !== '') {
+            borderColor(cells[a], '#06923E');
+            borderColor(cells[b], '#06923E');
+            borderColor(cells[c], '#06923E');
+            end = true;
+            msg.textContent = `${cells[a].textContent} wins !`;
+        }
+    }
+}
+
+// main
+function game() {
+    cells.forEach(cell => {
+        cell.addEventListener('click', () => {
+
+            if(end) return;
+
+            if(cell.textContent === '') {
+                cell.textContent = player;
+                cell.style.color = player === 'O' ? '#ff4d6d' : '#4d79ff';
+                player = player === 'O' ? 'X' : 'O';
+                checkWin();
+                draw();
+            }
+        })
+    })
+}
+
+game()
